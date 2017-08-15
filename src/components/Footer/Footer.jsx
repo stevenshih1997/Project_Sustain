2d8a188526107e7c29125233a95ab655bf7dc59b
Steven Shih <stevenshih1997@gmail.com>
2017-08-15 03:25:05 -0700

    added sponsor logo space

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,38 @@
-import React from 'react';
-import { Grid, NavItem, Nav } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { NavItem, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import Sponsors from './Sponsors';
+import Image1 from '../../images/imgcardtest.png';
 import './footer.css';
 
-function Footer(props) {
-  return (
-    <footer className="row-fluid footer">
-      <Nav justified>
-        <LinkContainer to="/">
-          <NavItem className="link__footer" >Privacy policy</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/">
-          <NavItem className="link__footer" title="Terms">Terms & Conditions</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/">
-          <NavItem className="link__footer" >Another professional link</NavItem>
-        </LinkContainer>
+class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      images: [Image1, Image1, Image1],
+    };
+  }
+  render() {
+    return (
+      <footer className="row-fluid footer">
+        <Sponsors images={this.state.images} />
+        <Nav justified>
+          <LinkContainer to="/">
+            <NavItem className="link__footer" >Privacy policy</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/">
+            <NavItem className="link__footer" title="Terms">Terms & Conditions</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/">
+            <NavItem className="link__footer" >Another professional link</NavItem>
+          </LinkContainer>
+        </Nav>
         <div className="text-center copyright">
           UBC 2017 ©
         </div>
-      </Nav>
-    </footer>
-  );
+      </footer>
+    );
+  }
 }
 
 export default Footer;

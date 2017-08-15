7a39fc691aaf7f393b6fc42f148f2730a0790177
Steven Shih <stevenshih1997@gmail.com>
2017-08-15 03:34:06 -0700

    removed carousel controls transition gradients

--- src/components/MainCarousel/MainCarousel.jsx
+++ src/components/MainCarousel/MainCarousel.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { Carousel, Image, Row } from 'react-bootstrap';
 import PlaceHolderImage from '../../images/tree.png';
+import './MainCarousel.css';
 
 function MainCarousel(props) {
   return (
     <Row>
       <Carousel>
         <Carousel.Item>
-          <img width={450} height={250} alt="900x500" src={PlaceHolderImage} />
+          <Image width={450} height={250} alt="900x500" src={PlaceHolderImage} />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img width={450} height={250} alt="900x500" src={PlaceHolderImage} />
+          <Image width={450} height={250} alt="900x500" src={PlaceHolderImage} />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img width={450} height={250} alt="900x500" src={PlaceHolderImage} />
+          <Image width={450} height={250} alt="900x500" src={PlaceHolderImage} />
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
